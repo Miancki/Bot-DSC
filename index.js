a47bda3d02eb2e986aba3b8af5441803777f9cbd
Miancki <126591563+Miancki@users.noreply.github.com>
2023-02-28 16:15:01 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const { Client, GatewayIntentBits, Partials } = require('discord.js')
+
+const { Guilds, GuildMembers, GuildMessages} = GatewayIntentBits;
+const { User, Message, GuildMember, ThreadMember, Channel} = Partials;
+
+const {loadEvents} = require('./Handlers/eventHandler.js')
+
+const client = new Client({
+  intents: [Guilds, GuildMembers, GuildMessages],
+  partials: [User, Message, GuildMember, ThreadMember],
+})
+
+client.config = require('./config.json');
+
+
+client.login(client.config.token).then(() => {
+  loadEvents(client);
+});
